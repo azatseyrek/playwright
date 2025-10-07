d3f1de3f3d979d2f050e1ba6055092934f539997
Azat Seyrek <azat.seyrek@adesso.com.tr>
2025-10-07 17:05:01 +0300

    Add: facing locators added to locators.spec.ts

--- tests/locators.spec.ts
+++ tests/locators.spec.ts
@@ -31,3 +31,12 @@ test('Locator syntax rules', async ({ page }) => {
   // by Exact text match
   page.locator(':text-is("Using the Grid")');
 });
+
+test('User facing locators', async ({ page }) => {
+  await page.getByRole('textbox', { name: 'Email' }).first().click();
+  await page.getByRole('button', { name: 'Sign in' }).first().click();
+  await page.getByLabel('Email').first().click();
+  await page.getByPlaceholder('Jane Doe').click();
+  await page.getByText('Using the Grid').click();
+  await page.getByTitle('IoT Dashboard').click();
+});
